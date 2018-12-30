1f61415411e324c2416b65970192fad28627a928
Milena Kari <milenakari@yahoo.com>
2018-12-30 00:00:43 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -54,5 +54,7 @@ class MusicLibraryController
    def list_songs_by_artist
      puts "Please enter the name of an artist:"
      user_input = gets.strip
+     artist = Artist.find_by_name(user_input)
+     binding.pry
    end
 end
